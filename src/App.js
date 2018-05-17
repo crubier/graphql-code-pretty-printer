2643f1eeb6d6a53d6fa22e8e98acb62e45a77613
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2018-05-17 22:54:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GraphqlCodeBlock } from "graphql-syntax-highlighter-react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "query { id name }"
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="MainTitle">GraphQL code pretty printer</h1>
+        <h2 className="LeftTitle">Input unformatted GraphQL:</h2>
+        <textarea
+          className="InputCode"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <h2 className="RightTitle">Output pretty-printer, formatted GraphQL:</h2>
+        <GraphqlCodeBlock
+          className="GraphqlCodeBlock"
+          queryBody={this.state.value}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
